dfd1851e05adcf6ef5d358d970611d45519e7177
Ester Pratt <esterpratt@gmail.com>
2019-01-29 12:42:44 +0200

    guess pic home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuessPicComponent } from './guessPic/guessPic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuessPicComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/guessPic/guessPic.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-guessPic-component',
+    templateUrl: './guessPic.component.html',
+    styleUrls: ['./guessPic.component.scss'],
+})
+export class GuessPicComponent {}
